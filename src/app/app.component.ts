fc338e65e4e53d95e5d4743cb0fbab8a2bd4d3e7
Patrick <114015928+PatrickSantanaaa@users.noreply.github.com>
2024-05-24 13:42:15 -0300

    criação tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:
+  [RouterOutlet],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
